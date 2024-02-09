a77a322d8d1fdd62ed110bd4de39ba2ffb586e62
matthew hairsnape <matthew@hairsnape.co.uk>
2024-02-09 13:25:13 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+for jpop in range(1, 21): 
+
+    if jpop % 3 == 0 and jpop % 5 == 0:  
+        print("FizzBuzz")
+
+    elif jpop % 3 == 0:  
+        print("Fizz")
+        
+    elif jpop % 5 == 0:  
+
+        print("Buzz")
+    else:
+        print(jpop)
