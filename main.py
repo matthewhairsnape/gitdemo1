fe9828b2a157900eff7491a0e87161f87bf091c6
matthew hairsnape <matthew@hairsnape.co.uk>
2024-02-09 14:14:03 +0000

    Commit 3 OOF

--- main.py
+++ main.py
@@ -14,3 +14,18 @@ for jpop in range(1, 21):
 
 
 #THIS COUNTS AS A STAGED COMMIT 
+        
+for jpop in range(1, 21): 
+
+    if jpop % 3 == 0 and jpop % 5 == 0:  
+        print("FizzBuzz")
+
+    elif jpop % 3 == 0:  
+        print("Fizz")
+        
+    elif jpop % 5 == 0:  
+
+        print("Buzz")
+    else:
+        print(jpop)
+
